913749517927dbc3b46d94ab015ac78848e3969b
Randy Zhu <mail@randyzhu.com>
2023-07-09 01:07:25 -0700

    make scenes move

--- Assets/OurGame/Script/DialogueButton.cs
+++ Assets/OurGame/Script/DialogueButton.cs
@@ -4,6 +4,11 @@ public class DialogueButton : MonoBehaviour
 {
     public GameObject[] background;
     private int _index;
+    
+    // timer for each dialogue background elem
+    // once timer is up, move to next dialogue background elem
+    int secondsBetweenDialogue = 10;
+    float timer = 0f;
 
 
     private void Start()
@@ -17,11 +22,19 @@ public class DialogueButton : MonoBehaviour
     {
     
         if (_index == 0) background[0].gameObject.SetActive(true);
+        timer += Time.deltaTime;
+        
+        if (timer > secondsBetweenDialogue)
+        {
+            Next();
+        }
+        // 
     }
 
     public void Next()
     {
-        
+        timer = 0f;
+
         for (var i = 0; i < background.Length; i++)
         {
             background[i].gameObject.SetActive(false);
